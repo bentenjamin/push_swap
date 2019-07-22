7049be0eb9695d19e0e5c9d0ce7daa0566b0d27e
Benjamin WEBB <bwebb@c3r4s4.wethinkcode.co.za>
2019-07-22 14:35:00 +0200

    progress i guess, it compiles but isnt printing correctly

--- checker.c
+++ checker.c
@@ -1,14 +1,23 @@
 #include "ps.h"
-//write atol, stkadd. fix main
-int main(int argc, char **argv)
-{
-    t_flgs  flgs;
-    t_ps    stka;
-    int i;
+// fix main
 
-    if (argc == 1)
-        exit(0);
-    args = chkargs()
+void    ft_freestck(t_ps **stck)
+{
+    if (*stck)
+    {
+        if ((*stck)->xt != NULL)
+            ft_freestck(&((*stck)->xt));
+        free(*stck);
+    }
+}
+
+void    ft_vstk(t_ps **stck)
+{
+    while (*stck)
+    {
+        ft_putnbr((*stck)->num);
+        *stck = (*stck)->xt;
+    }
 }
 
 void    ft_initflgs(t_flgs **flgs)
@@ -29,29 +38,71 @@ void    ft_setflgs(char *s, t_flgs **flgs)
             (*flgs)->v = 1;
 }
 
+int ft_stkadd(int num, t_ps **stck)
+{
+    t_ps    *prevnode;
+
+    prevnode = NULL;
+    while (*stck)
+    {
+        prevnode = *stck;
+        *stck = (*stck)->xt;
+    }
+    if (!(*stck = (t_ps *)malloc(sizeof(t_ps))))
+        return (0);
+    (*stck)->num = num;
+    (*stck)->xt = NULL;
+    if (prevnode)
+        prevnode->xt = *stck;
+    return (1);
+}
+
+void    ft_abrt()
+{
+    
+}
+
 int ft_rd(char **arr, t_ps **stka, t_flgs **flgs)
 {
     int     i;
+    int     k;
+    char    **splt;
 
     i = 0;
     while (arr[i])
     {
         if (ft_strchr(arr[i], ' '))
-            ft_rd(ft_strsplit(arr[i], ' '), stka, flgs);
+        {
+            k = 0;
+            splt = ft_strsplit(arr[i], ' ');
+            ft_rd(splt, stka, flgs);
+            while (splt[k])
+                free(splt[k++]);
+            free(splt);
+        }
         else if (arr[i][0] == '-')
             ft_setflgs(arr[i], flgs);
-        else if (!(ft_isonly(arr[i], ft_isdigit)))
-            ft_abt;
-        else if (ft_atol(arr[i]) > INT32_MAX)
-            ft_abt;
-        else
-            ft_stkadd(ft_atoi(arr[i]), stka);
+        else if ((!(ft_isonly(arr[i], ft_isdigit))) || (ft_atol(arr[i]) > \
+                INT32_MAX) || (!(ft_stkadd(ft_atoi(arr[i]), stka))))
+            return (0);
     i++;
     }
+    return (1);
 }
 
-void    ft_abt()
+int main(int argc, char **argv)
 {
-    ft_putstr_fd("Error\n", 2);
-    exit(1);
+    t_flgs  *flgs;
+    t_ps    *stka;
+
+    if (argc == 1)
+        exit(0);
+    flgs = (t_flgs *)malloc(sizeof(t_flgs));
+    if (!(ft_rd(++argv, &stka, &flgs)))
+    {
+        ft_putstr_fd("Error\n", 2);
+        exit(1);
+    }
+    ft_vstk(&stka);
+    ft_freestck(&stka);
 }
--- ps.h
+++ ps.h
@@ -4,14 +4,14 @@
 
 typedef struct  s_ps
 {
-    int     num;
-    char    *xt;
-}           t_ps;
+    int             num;
+    struct s_ps    *xt;
+}                   t_ps;
 
 typedef struct  s_flgs
 {
-    char    v : 1;
-    char    c : 1;
-}           t_flgs;
+    char            v : 1;
+    char            c : 1;
+}                   t_flgs;
 
 #endif
